824dc5c124e369eaba6f2c172995808b596c7b7d
KayoGates <boydalvin94@gmail.com>
2023-06-30 22:41:59 -0500

    updated Vehicle class

--- CarOo.js
+++ CarOo.js
@@ -12,4 +12,44 @@ class Vehicle
 
 }
 
+class Car extends Vehicle
+{
+    constructor(make, model, year, numWheels)
+    {
+        super(make, model, year, numWheels);
+        this.numWheels = 4;
+    }
+}
 
+
+class Motorcycle extends Vehicle
+{
+    constructor(make, model, year, numWheels)
+    {
+        super(make, model, year, numWheels);
+        this.numWheels = 2;
+    }
+
+    revEngine(){return "VROOM!";}
+}
+
+class Garage extends Vehicle
+{
+    constructor(vehicles, capacity)
+    {
+        this.vehicles = [];
+        this.capacity = capacity;
+    }
+
+    add(newVehicle)
+    {
+        if (!(newVehicle instanceof Vehicle)) {
+            return "Only vehicles are allowed in here!";
+          }
+          if (this.vehicles.length >= this.capacity) {
+            return "Sorry, we're full.";
+          }
+          this.vehicles.push(newVehicle);
+          return "Vehicle added!";
+    }
+}
